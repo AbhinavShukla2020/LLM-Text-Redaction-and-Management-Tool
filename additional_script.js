185583e06248a2e9b4086894629b087543d4c452
AbhinavShukla2020 <64708297+AbhinavShukla2020@users.noreply.github.com>
2024-08-08 11:26:39 -0700

    Update additional_script.js

--- additional_script.js
+++ additional_script.js
@@ -1,7 +1,14 @@
 <!--
     START OF ADDITIONAL SCRIPTS
 -->
-
+<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/js/select2.min.js"></script>
+<script>
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed.";
+  }
+</script>
     <!-- CSS Styles for the modal and buttons -->
      <style>
         /* Modal overlay */
